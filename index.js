26673793755a746803c4c2425ec79382d9c50140
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-07-01 12:28:29 +0530

    fix: show both headings

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello from my CI/CD demo to Github Actions!</h1>');
+  res.write('<h1>Hello from my CI/CD demo to Github Actions!</h1>');
   res.end('<h2>Welcome to my CI/CD demo!</h2>');
 });
 
